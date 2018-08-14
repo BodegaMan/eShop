1aae8bc64de160f20acb914a743ce0bc2d7ed55f
Bodega Man <42325549+BodegaMan@users.noreply.github.com>
2018-08-14 15:29:33 -0500

    Basket Service Part 2 Completed

---
+++ eShop.Core/Contracts/IBasketService.cs
@@ -0,0 +1,20 @@
+﻿using eShop.Core.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace eShop.Core.Contracts
+{
+    public interface IBasketService
+    {
+        void AddToBasket(HttpContextBase httpContext, string productId);
+        void RemoveFromBasket(HttpContextBase httpContext, string itemId);
+        List<BasketItemViewModel> GetBasketItems(HttpContextBase httpContext);
+        BasketSummaryViewModel GetBasketSummary(HttpContextBase httpContext);
+
+
+    }
+}
