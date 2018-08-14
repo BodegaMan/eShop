3e5641a3e4e5437c9929ffe2aec5fbd70c66d117
Bodega Man <42325549+BodegaMan@users.noreply.github.com>
2018-08-14 13:35:40 -0500

    Basket Model Completed

---
+++ eShop.Core/Models/Basket.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eShop.Core.Models
+{
+    public class Basket : BaseEntity
+    {
+        // This is what is known as Lazy Loading
+        public virtual ICollection<BasketItem> BasketItems { get; set; }
+
+        public Basket()
+        {
+            this.BasketItems = new List<BasketItem>();
+        }
+    }
+}
